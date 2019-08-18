d5ebd9ddfd87aaba5aa03fc1c3fd6ec0e048dddf
Alex Griffin <a@ajgrf.com>
2019-08-18 09:04:05 -0500

    s/Emmacs/Emacs/

--- edwin.el
+++ edwin.el
@@ -8,7 +8,7 @@
 ;;; Copyright © 2019 Alex Griffin <a@ajgrf.com>
 ;;;
 ;;;
-;;; This file is NOT part of GNU Emmacs.
+;;; This file is NOT part of GNU Emacs.
 ;;;
 ;;; This program is free software: you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
